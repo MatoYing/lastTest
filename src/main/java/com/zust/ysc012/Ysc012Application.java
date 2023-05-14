9016dd921d5dbefb3a31bebaee89ff3ec29c41be
Mato <1350478903@qq.com>
2023-05-14 16:01:54 +0800

    first

---
+++ src/main/java/com/zust/ysc012/Ysc012Application.java
@@ -0,0 +1,14 @@
+package com.zust.ysc012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 程序的主入口，相当于配置类
+@SpringBootApplication
+public class Ysc012Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ysc012Application.class, args);
+    }
+
+}
